c8be01e5939389d8460cf186fc3250b4e042f108
maryummurtaza345 <45827724+maryummurtaza345@users.noreply.github.com>
2018-12-16 10:55:28 -0800

    Add files via upload
    complete website changes adn a homepage with a navigation bar.

---
+++ last page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> PHP</title>
+<style>
+
+</style>
+
+</head>
+<body>
+<H1>PHP</H1>
+<p>
+<?php
+ echo "PHP is a hypertext preprocessor pr it is a server-side scripting language designed for Web Delopment.It was originally created by Rasmus Lerdfordin 1994."
+?>
+</p>
+<p>
+<?php
+echo"PHP code can be embedded into HTML code, or it can be used in combination with various web template systems, web content management systems, and web frameworks.PHP code is usually interpret by PHP interpreter implemented as a  module in the web server or as a common gateway interface executeable.The web serveers cmbines the results of the interpreted and executed PHP code, which may be any type of data, including images, with the generated web page.PHP code may be also be executed with the command line interface(CLI) and can be used to implemnt standalone graphical applications."
+?>
+</p>
+
+
+
+
+
+</body>
+</html>
